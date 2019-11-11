6176583e9b8631956bdf484df05a01ad57b510b0
john25541 <nguyenvanloc1247@gmail.com>
2019-11-11 18:48:23 +0700

    add all file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,7 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
-
-        redirect_to article_path(@aricle)
+        redirect_to article_path(@article)
     end
     
     private
